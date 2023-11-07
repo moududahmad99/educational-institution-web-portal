ff0d2f67b1109f1bd9c84887a004e778617a5e28
saju575 <sakilahmedsaju575@gmail.com>
2023-11-08 00:44:48 +0600

    admission notice added

--- src/pages/dropdown/admissions/admissionExamSyllabus/AdmissionExamSyllabus.jsx
+++ src/pages/dropdown/admissions/admissionExamSyllabus/AdmissionExamSyllabus.jsx
@@ -1,92 +1,147 @@
-import React, { useState } from 'react'
-import { BsFillCalendarDateFill } from 'react-icons/bs';
-import { PiDownloadSimpleBold } from 'react-icons/pi';
-import { AiFillEye } from 'react-icons/ai';
-import Modal from 'react-modal';
+import moment from "moment/moment";
+import React from "react";
+import { AiFillEye } from "react-icons/ai";
+import { BsFillCalendarDateFill } from "react-icons/bs";
+import { PiDownloadSimpleBold } from "react-icons/pi";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useInfiniteQuery } from "react-query";
+import { Link } from "react-router-dom";
+import ErrorMsg from "../../../../components/errorMsg/ErrorMsg";
+import Spinner from "../../../../components/spinner/Spinner";
+import { getAllNews } from "../../../../utills/getAllNews";
 
 const AdmissionExamSyllabus = () => {
-  
-  const tableRows = [
-    { date: '10 - 1 - 2023', content: 'ভর্তি পরিক্ষার সিলেবাস ২০২৩' },
-    { date: '11 - 10 - 2022', content: 'ভর্তি পরিক্ষার সিলেবাস ২০২২' },
-  ];
+  /* 
+    get  all the  data
+  */
+  const { data, isError, error, isLoading, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      staleTime: Infinity,
+      queryKey: ["admission_syllabus"],
+      queryFn: ({ pageParam }) =>
+        getAllNews({ page: pageParam, limit: 10, type: "admission syllabus" }),
+      getNextPageParam: (lastPage) => {
+        if (lastPage.payload.currentPage < lastPage.payload.totalPages) {
+          return lastPage.payload.currentPage + 1;
+        } else {
+          return false;
+        }
+      },
+    });
 
-  // Modal 
-  const [isPdfModalOpen, setPdfModalOpen] = useState(false);
-
-  // Function to open the PDF modal
-  const openPdfModal = () => {
-    setPdfModalOpen(true);
-  };
-
-  // Function to close the PDF modal
-  const closePdfModal = () => {
-    setPdfModalOpen(false);
-  };
+  /* 
+      store the all page data in the one array to scroll infinitely
+    */
+  const admission_syllabus_data = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.payload.news];
+  }, []);
 
   return (
     <React.Fragment>
       <div id="admissionExamSyllabus">
         <div className="pages-title my-4 bg-gradient-to-r from-[rgba(244,254,255,1)] to-[rgba(161,182,182,1)] text-center flex flex-col items-center py-4">
-          <h3 className='bg-[#79929C] text-lg font-medium my-2 text-white px-14 sm:px-14 py-2'>ভর্তি পরিক্ষার সিলেবাস</h3>
-          <p className='text-md font-medium mt-2'>home / admissions / <span className='text-red-800'> admission-exam-syllabus</span></p>
+          <h3 className="bg-[#79929C] text-lg font-medium my-2 text-white px-14 sm:px-14 py-2">
+            ভর্তি পরিক্ষার সিলেবাস
+          </h3>
         </div>
 
         <div className="border vertical-scrollMain bg-[#DBE8E960]">
-          <table className="border-collapse w-full vertical-scroll">
+          {/* 
+          render data
+        */}
+          {admission_syllabus_data?.length > 0 && (
+            <InfiniteScroll
+              dataLength={
+                admission_syllabus_data?.length > 0
+                  ? admission_syllabus_data.length
+                  : 0
+              }
+              next={fetchNextPage}
+              hasMore={hasNextPage}
+              loader={
+                <div className="flex justify-center items-center py-4">
+                  <Spinner />
+                </div>
+              }
+            >
+              <table className="border-collapse w-full vertical-scroll">
+                <thead>
+                  <tr className="bg-[#BBCDCD60]">
+                    <th className="p-2 text-start w-3/12">প্রকাশের তারিখ</th>
+                    <th className="p-2 text-start w-5/12">নোটিশ</th>
+                    <th className="p-2 text-start w-2/12">ভিউ [PDF]</th>
+                    <th className="p-2 text-start w-2/12">ডাউনলোড [PDF]</th>
+                  </tr>
+                </thead>
 
-            <thead>
-              <tr className="bg-[#BBCDCD60] ">
-                <th className="p-2 text-start w-3/12">প্রকাশের তারিখ</th>
-                <th className="p-2 text-start w-5/12">নোটিশ</th>
-                <th className="p-2 text-start w-2/12">ভিউ [PDF]</th>
-                <th className="p-2 text-start w-2/12">ডাউনলোড [PDF]</th>
-              </tr>
-            </thead>
+                <tbody>
+                  {admission_syllabus_data.map((row, index) => (
+                    <tr key={index} className="border-b">
+                      <div className="flex items-center">
+                        <td className="p-2">
+                          <i>
+                            <BsFillCalendarDateFill />
+                          </i>
+                        </td>
+                        <td className="py-2">
+                          {moment(row.updatedAt).format("DD - MM - YYYY")}
+                        </td>
+                      </div>
+                      <td className="p-2">
+                        {row.title.split(/\s+/).slice(0, 13).join(" ") + "..."}
+                      </td>
+                      <td className="p-2">
+                        <Link
+                          to={`/notice/${row._id}`}
+                          className="flex items-center"
+                        >
+                          {" "}
+                          <i className="pr-1">
+                            <AiFillEye />
+                          </i>{" "}
+                          View
+                        </Link>
+                      </td>
+                      <td className="p-2">
+                        <a
+                          href={`#download-link-${index}`} //Todo: download task
+                          className="py-2 flex items-center"
+                        >
+                          <i className="pr-1">
+                            <PiDownloadSimpleBold />
+                          </i>{" "}
+                          Download
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </InfiniteScroll>
+          )}
 
-            <tbody>
-              {tableRows.map((row, index) => (
-                <tr key={index} className="border-b">
-                  <div className='flex items-center pt-2'>
-                    <td className="p-2"><i><BsFillCalendarDateFill /></i></td>
-                    <td className="py-2">{row.date}</td>
-                  </div>
-                  <td className="p-2">{row.content}</td>
-                  <td className="p-2">
-                    <a href={`#view-link-${index}`} onClick={openPdfModal} className='flex items-center'> <i className='pr-1'><AiFillEye /></i> View</a>
-                  </td>
-                  <td className="p-2">
-                    <a href={`#download-link-${index}`} className='py-2 flex items-center'>
-                      <i className='pr-1'><PiDownloadSimpleBold /></i> Download
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-
-          </table>
-
-          <Modal
-            isOpen={isPdfModalOpen}
-            onRequestClose={closePdfModal}
-            contentLabel="PDF Viewer"
-          >
-            <button onClick={closePdfModal} className='flex flex-end right-2 top-2'>Close</button>
-            <iframe
-              src="https://www.africau.edu/images/default/sample.pdf"
-              title="PDF Viewer"
-              width="100%"
-              height="700"
-              frameBorder="0"
-              scrolling="auto"
-            ></iframe>
-          </Modal>
-
+          {/* 
+          showing error messages and loading
+          */}
+          {isLoading && (
+            <div className="flex justify-center items-center py-4">
+              <Spinner />
+            </div>
+          )}
+          {!isLoading && admission_syllabus_data?.length === 0 && (
+            <div className="flex justify-center items-center py-4">
+              <ErrorMsg msg="No data found" />
+            </div>
+          )}
+          {isError && (
+            <div className="flex justify-center items-center py-4">
+              <ErrorMsg msg={error.message} />
+            </div>
+          )}
         </div>
       </div>
-      
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default AdmissionExamSyllabus
+export default AdmissionExamSyllabus;
--- src/pages/dropdown/admissions/admissionNotice/AdmissionNotice.jsx
+++ src/pages/dropdown/admissions/admissionNotice/AdmissionNotice.jsx
@@ -1,91 +1,145 @@
-import React, { useState } from 'react';
-import { BsFillCalendarDateFill } from 'react-icons/bs';
-import { PiDownloadSimpleBold } from 'react-icons/pi';
-import { AiFillEye } from 'react-icons/ai';
-import Modal from 'react-modal';
+import moment from "moment/moment";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import { AiFillEye } from "react-icons/ai";
+import { BsFillCalendarDateFill } from "react-icons/bs";
+import { PiDownloadSimpleBold } from "react-icons/pi";
+import { useInfiniteQuery } from "react-query";
+import { Link } from "react-router-dom";
+import ErrorMsg from "../../../../components/errorMsg/ErrorMsg";
+import Spinner from "../../../../components/spinner/Spinner";
+import { getAllNews } from "../../../../utills/getAllNews";
 
 const AdmissionNotice = () => {
+  /* 
+    get  all the  data
+  */
+  const { data, isError, error, isLoading, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      staleTime: Infinity,
+      queryKey: ["admission_circular"],
+      queryFn: ({ pageParam }) =>
+        getAllNews({ page: pageParam, limit: 10, type: "admission circular" }),
+      getNextPageParam: (lastPage) => {
+        if (lastPage.payload.currentPage < lastPage.payload.totalPages) {
+          return lastPage.payload.currentPage + 1;
+        } else {
+          return false;
+        }
+      },
+    });
 
-  const tableRows = [
-    { date: '10 - 1 - 2023', content: 'সাম্প্রতিক বিজ্ঞপ্তি পরীক্ষা ফর্ম' },
-    { date: '10 - 9 - 2015', content: 'বিশেষ প্রশাসনিক কর্মচারী নিয়োগ।' },
-    { date: '11 - 10 - 2013', content: 'প্রত্যায়ন পরীক্ষা আবেদন ফর্ম' },
-    { date: '11 - 10 - 2013', content: 'প্রাথমিক শিক্ষক নিয়োগ পরীক্ষা' },
-  ];
-
-  // Modal 
-  const [isPdfModalOpen, setPdfModalOpen] = useState(false);
-
-  // Function to open the PDF modal
-  const openPdfModal = () => {
-    setPdfModalOpen(true);
-  };
-
-  // Function to close the PDF modal
-  const closePdfModal = () => {
-    setPdfModalOpen(false);
-  };
+  /* 
+      store the all page data in the one array to scroll infinitely
+    */
+  const admission_circular_data = data?.pages.reduce((acc, page) => {
+    return [...acc, ...page.payload.news];
+  }, []);
 
   return (
     <div id="admissionNotice">
       <div className="pages-title my-4 bg-gradient-to-r from-[rgba(244,254,255,1)] to-[rgba(161,182,182,1)] text-center flex flex-col items-center py-4">
-        <h3 className='bg-[#79929C] text-lg font-medium my-2 text-white px-14 sm:px-14 py-2'>ভর্তি বিজ্ঞপ্তি</h3>
-        <p className='text-md font-medium mt-2'>home / admissions / <span className='text-red-800'>admission-notice</span></p>
+        <h3 className="bg-[#79929C] text-lg font-medium my-2 text-white px-14 sm:px-14 py-2">
+          ভর্তি বিজ্ঞপ্তি
+        </h3>
       </div>
 
+      {/* 
+      render data
+    */}
       <div className="border vertical-scrollMain bg-[#DBE8E960]">
-        <table className="border-collapse w-full vertical-scroll">
+        {admission_circular_data?.length > 0 && (
+          <InfiniteScroll
+            dataLength={
+              admission_circular_data?.length > 0
+                ? admission_circular_data.length
+                : 0
+            }
+            next={fetchNextPage}
+            hasMore={hasNextPage}
+            loader={
+              <div className="flex justify-center items-center py-4">
+                <Spinner />
+              </div>
+            }
+          >
+            <table className="border-collapse w-full vertical-scroll">
+              <thead>
+                <tr className="bg-[#BBCDCD60]">
+                  <th className="p-2 text-start w-3/12">প্রকাশের তারিখ</th>
+                  <th className="p-2 text-start w-5/12">নোটিশ</th>
+                  <th className="p-2 text-start w-2/12">ভিউ [PDF]</th>
+                  <th className="p-2 text-start w-2/12">ডাউনলোড [PDF]</th>
+                </tr>
+              </thead>
 
-          <thead>
-            <tr className="bg-[#BBCDCD60]">
-              <th className="p-2 text-start w-3/12">প্রকাশের তারিখ</th>
-              <th className="p-2 text-start w-5/12">নোটিশ</th>
-              <th className="p-2 text-start w-2/12">ভিউ [PDF]</th>
-              <th className="p-2 text-start w-2/12">ডাউনলোড [PDF]</th>
-            </tr>
-          </thead>
+              <tbody>
+                {admission_circular_data.map((row, index) => (
+                  <tr key={index} className="border-b">
+                    <div className="flex items-center">
+                      <td className="p-2">
+                        <i>
+                          <BsFillCalendarDateFill />
+                        </i>
+                      </td>
+                      <td className="py-2">
+                        {moment(row.updatedAt).format("DD - MM - YYYY")}
+                      </td>
+                    </div>
+                    <td className="p-2">
+                      {row.title.split(/\s+/).slice(0, 13).join(" ") + "..."}
+                    </td>
+                    <td className="p-2">
+                      <Link
+                        to={`/notice/${row._id}`}
+                        className="flex items-center"
+                      >
+                        {" "}
+                        <i className="pr-1">
+                          <AiFillEye />
+                        </i>{" "}
+                        View
+                      </Link>
+                    </td>
+                    <td className="p-2">
+                      <a
+                        href={`#download-link-${index}`} //Todo: download task
+                        className="py-2 flex items-center"
+                      >
+                        <i className="pr-1">
+                          <PiDownloadSimpleBold />
+                        </i>{" "}
+                        Download
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </InfiniteScroll>
+        )}
 
-          <tbody>
-            {tableRows.map((row, index) => (
-              <tr key={index} className="border-b">
-                <div className='flex items-center'>
-                  <td className="p-2"><i><BsFillCalendarDateFill /></i></td>
-                  <td className="py-2">{row.date}</td>
-                </div>
-                <td className="p-2">{row.content}</td>
-                <td className="p-2">
-                  <a href={`#view-link-${index}`} onClick={openPdfModal} className='flex items-center'> <i className='pr-1'><AiFillEye /></i> View</a>
-                </td>
-                <td className="p-2">
-                  <a href={`#download-link-${index}`} className='py-2 flex items-center'>
-                    <i className='pr-1'><PiDownloadSimpleBold /></i> Download
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-
-        </table>
-
-        <Modal
-          isOpen={isPdfModalOpen}
-          onRequestClose={closePdfModal}
-          contentLabel="PDF Viewer"
-        >
-          <button onClick={closePdfModal} className='flex flex-end right-2 top-2'>Close</button>
-          <iframe
-            src="https://www.africau.edu/images/default/sample.pdf"
-            title="PDF Viewer"
-            width="100%"
-            height="700"
-            frameBorder="0"
-            scrolling="auto"
-          ></iframe>
-        </Modal>
-
+        {/* 
+          showing error messages and loading
+          */}
+        {isLoading && (
+          <div className="flex justify-center items-center py-4">
+            <Spinner />
+          </div>
+        )}
+        {!isLoading && admission_circular_data?.length === 0 && (
+          <div className="flex justify-center items-center py-4">
+            <ErrorMsg msg="No data found" />
+          </div>
+        )}
+        {isError && (
+          <div className="flex justify-center items-center py-4">
+            <ErrorMsg msg={error.message} />
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default AdmissionNotice;
