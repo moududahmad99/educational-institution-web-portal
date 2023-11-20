de7fe083bb0f8eab13f10d21ec15c89c90acbe83
saju575 <sakilahmedsaju575@gmail.com>
2023-11-20 21:31:26 +0600

    spinner problem fix

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -3,7 +3,7 @@ import styles from "./spinner.module.css";
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center py-4">
-      <span className={`${styles.loader}`}></span>;
+      <span className={`${styles.loader}`}></span>
     </div>
   );
 };
